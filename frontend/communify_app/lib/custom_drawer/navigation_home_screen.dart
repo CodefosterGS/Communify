4f08e4ebe8dc7f43d0cf16aa4f3189043b88c4fd
AjinkyaTaranekar <ajinkyataranekar@gmail.com>
2019-12-03 18:57:17 +0530

    added side drawer

---
+++ frontend/communify_app/lib/custom_drawer/navigation_home_screen.dart
@@ -0,0 +1,77 @@
+import 'package:communify_app/app_theme.dart';
+import 'package:communify_app/custom_drawer/drawer_user_controller.dart';
+import 'package:communify_app/custom_drawer/home_drawer.dart';
+import 'package:communify_app/custom_drawer/feedback_screen.dart';
+import 'package:communify_app/custom_drawer/help_screen.dart';
+import 'package:communify_app/custom_drawer/invite_friend_screen.dart';
+import 'package:flutter/material.dart';
+
+import '../home_screen.dart';
+
+class NavigationHomeScreen extends StatefulWidget {
+  @override
+  _NavigationHomeScreenState createState() => _NavigationHomeScreenState();
+}
+
+class _NavigationHomeScreenState extends State<NavigationHomeScreen> {
+  Widget screenView;
+  DrawerIndex drawerIndex;
+  AnimationController sliderAnimationController;
+
+  @override
+  void initState() {
+    drawerIndex = DrawerIndex.HOME;
+    screenView = const HomeScreen();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: AppTheme.nearlyWhite,
+      child: SafeArea(
+        top: false,
+        bottom: false,
+        child: Scaffold(
+          backgroundColor: AppTheme.nearlyWhite,
+          body: DrawerUserController(
+            screenIndex: drawerIndex,
+            drawerWidth: MediaQuery.of(context).size.width * 0.75,
+            animationController: (AnimationController animationController) {
+              sliderAnimationController = animationController;
+            },
+            onDrawerCall: (DrawerIndex drawerIndexdata) {
+              changeIndex(drawerIndexdata);
+            },
+            screenView: screenView,
+          ),
+        ),
+      ),
+    );
+  }
+
+  void changeIndex(DrawerIndex drawerIndexdata) {
+    if (drawerIndex != drawerIndexdata) {
+      drawerIndex = drawerIndexdata;
+      if (drawerIndex == DrawerIndex.HOME) {
+        setState(() {
+          screenView = const HomeScreen();
+        });
+      } else if (drawerIndex == DrawerIndex.Help) {
+        setState(() {
+          screenView = HelpScreen();
+        });
+      } else if (drawerIndex == DrawerIndex.FeedBack) {
+        setState(() {
+          screenView = FeedbackScreen();
+        });
+      } else if (drawerIndex == DrawerIndex.Invite) {
+        setState(() {
+          screenView = InviteFriend();
+        });
+      } else {
+        //do in your way......
+      }
+    }
+  }
+}
