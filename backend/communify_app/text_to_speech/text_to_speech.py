ab998fe2ee431b9560d2c6ffd52c6d25d2c6798f
AjinkyaTaranekar <ajinkyataranekar@gmail.com>
2019-12-06 12:03:49 +0530

    Update text_to_speech.py

--- backend/communify_app/text_to_speech/text_to_speech.py
+++ backend/communify_app/text_to_speech/text_to_speech.py
@@ -1,16 +1,25 @@
+from flask import Flask, redirect, url_for, request 
 import pyttsx3 
+app = Flask(__name__) 
 
 converter = pyttsx3.init() 
+converter.setProperty('rate', 150) 
+converter.setProperty('volume', 0.7) 
 
-def adjustSpeed():
-    converter.setProperty('rate', 150) 
+@app.route('/success/<name>') 
+def success(name): 
+    converter.say(name)
+    converter.runAndWait()
+    return "welcome"
 
-def adjustVolume():
-    converter.setProperty('volume', 0.7) 
+@app.route('/login',methods = ['POST', 'GET']) 
+def login(): 
+    if request.method == 'POST': 
+	    user = request.form['nm'] 
+	    return redirect(url_for('success',name = user)) 
+    else: 
+	    user = request.args.get('nm') 
+	    return redirect(url_for('success',name = user)) 
 
-def say():
-    converter.say(input) 
- 
-converter.runAndWait()
-
-
+if __name__ == '__main__': 
+    app.run(debug = True) 
